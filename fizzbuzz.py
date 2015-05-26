acaf347654c0f1fbcb4126d189a5beedf63d0459
Paolo Zamberlan <pzamberlan@gmail.com>
2015-05-26 15:21:11 -0300

    explicacion del codigo

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in xrange(1, 101):
-    if i % 15 == 0:
+for i in xrange(1, 101): #cuenta los numeros del 1 al 100
+    if i % 15 == 0: #si el resto de la division es = a 0 imprime fizzbuzz
         print "FizzBuzz"
-    elif i % 3 == 0:
+    elif i % 3 == 0:#si el resto de la division es = a 0 imprime fizz
         print "Fizz"
-    elif i % 5 == 0:
+    elif i % 5 == 0:#si el resto de la division es = a 0 imprime buzz
         print "Buzz"
     else:
         print i
